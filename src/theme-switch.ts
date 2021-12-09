8d4e26a6fc982e3bc10d8141073377073f7576f1
Tony Spegel <TonySpegel@googlemail.com>
2021-12-09 20:06:24 +0100

    initial styles and markup

--- src/theme-switch.ts
+++ src/theme-switch.ts
@@ -5,10 +5,14 @@
  */
 
 import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
+
+// const savePreference = (theme: string): void => {
+//     localStorage.setItem('theme-preference', theme);
+// };
 
 /**
- * An example element.
+ * Custom element which helps switching themes.
  *
  * @fires count-changed - Indicates when the count changes
  * @slot - This element has a slot
@@ -16,53 +20,95 @@ import {customElement, property} from 'lit/decorators.js';
  */
 @customElement('theme-switch')
 export class ThemeSwitch extends LitElement {
-  static override styles = css`
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
-    }
-  `;
+    static override styles = css`
+        :host {
+            display: block;
+            border: solid 1px gray;
+            padding: calc(var(--base-gap));
+            min-width: 340px;
 
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World';
+            --base-gap: 8px;
+            --base-radius: 8px;
+        }
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+        #btn-theme-selection {
+            border-radius: 50%;
+            width: 25px;
+            aspect-ratio: 1;
 
-  override render() {
-    return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
+            color: var(--color, var(--color));
+        }
+
+        #dialog-theme-selection {
+            border: 1px solid darkorchid;
+            border-radius: var(--base-radius);
+            padding: var(--base-gap);
+        }
+
+        #dialog-theme-selection[aria-hidden='true'] {
+            display: none;
+        }
     `;
-  }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
-  }
+    /**
+     * The name to say "Hello" to.
+     */
+    @property({type: String})
+    name = 'World';
 
-  /**
-   * Formats a greeting
-   * @param name The name to say "Hello" to
-   */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
-  }
+    /**
+     * The number of times the button has been clicked.
+     */
+    @property({type: Number})
+    count = 0;
+
+    @state()
+    private dialogHidden = true;
+
+    override render() {
+        return html`
+            <!-- <h1>${this.sayHello(this.name)}!</h1>
+            <button @click=${this._onClick} part="button">
+                Click Count: ${this.count}
+            </button> -->
+
+            <button
+                @click=${() => (this.dialogHidden = !this.dialogHidden)}
+                aria-label="Theme-Auswahl öffnen"
+                id="btn-theme-selection"
+                title="Theme-Auswahl öffnen"
+            ></button>
+            <div
+                aria-hidden="${this.dialogHidden}"
+                aria-label="Theme-Auswahl"
+                aria-modal="true"
+                id="dialog-theme-selection"
+                role="dialog"
+            >
+                <input type="radio" name="themes" value="day" />
+                <input type="radio" name="themes" value="night" />
+                <span>Dialog Content</span>
+            </div>
+            <!-- <slot></slot> -->
+        `;
+    }
+
+    private _onClick() {
+        this.count++;
+        this.dispatchEvent(new CustomEvent('count-changed'));
+    }
+
+    /**
+     * Formats a greeting
+     * @param name The name to say "Hello" to
+     */
+    sayHello(name: string): string {
+        return `Hello, ${name}`;
+    }
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'theme-switch': ThemeSwitch;
-  }
+    interface HTMLElementTagNameMap {
+        'theme-switch': ThemeSwitch;
+    }
 }
